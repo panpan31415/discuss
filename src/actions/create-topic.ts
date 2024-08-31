c868b401d2b20746c539fff383c5a2abcdbb02d7
panpan <panpan31415@gmail.com>
2024-08-31 21:22:08 +0200

    add zod to validate form

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,8 +1,46 @@
 "use server";
 
-export async function createTopic(formData: FormData) {
-    const name = formData.get("name");
-    const description = formData.get("description");
+import { auth } from "@/auth";
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+    name: z
+        .string()
+        .min(2)
+        .regex(/^[a-zA-Z-. #]+$/),
+    description: z.string().min(10, { message: "at least 10 characters" }),
+});
+
+type FormState = {
+    succeeded?: boolean;
+    errors?: {
+        name?: string[];
+        description?: string[];
+        _form?: string[];
+    };
+};
+
+export async function createTopic(formState: FormState, formData: FormData): Promise<FormState> {
+    const result = createTopicSchema.safeParse({
+        name: formData.get("name"),
+        description: formData.get("description"),
+    });
+    if (!result.success) {
+        return { errors: result.error.flatten().fieldErrors };
+    }
+    const { name, description } = result.data;
     console.log(name, description);
+    const session = await auth();
+    if (!session?.user) {
+        return {
+            errors: {
+                _form: ["Please sign in"],
+            },
+        };
+    }
+    // insert db
+    return {
+        succeeded: true,
+    };
     // TODO: revalidate the homepage
 }
