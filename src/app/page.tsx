71594075a1923b90cf0a29270914efb5d4e042ae
panpan <panpan31415@gmail.com>
2024-08-31 16:20:40 +0200

    remove auth test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-import { signIn, signOut } from "@/actions";
-import { auth } from "@/auth";
-import { Button } from "@nextui-org/react";
-
 export default async function Home() {
-    const session = await auth();
-    return (
-        <div>
-            <form action={signIn}>
-                <Button type='submit'>Sign in </Button>
-            </form>
-
-            <form action={signOut}>
-                <Button type='submit'>Sign out</Button>
-            </form>
-
-            {session?.user ? <p>you are signed in as {session.user.name}</p> : <p>you are signed out</p>}
-        </div>
-    );
+    return <div>Home page</div>;
 }
