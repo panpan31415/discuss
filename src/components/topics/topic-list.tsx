f4f4949022838609e75f5115879d87770e9b3884
panpan <panpan31415@gmail.com>
2024-09-02 12:40:12 +0200

    topic list completed

---
+++ src/components/topics/topic-list.tsx
@@ -0,0 +1,21 @@
+import paths from "@/path";
+import { Chip } from "@nextui-org/react";
+import Link from "next/link";
+
+export default async function TopicList() {
+    const topics = await prisma?.topic.findMany();
+    const renderedTopics = topics?.map((topic) => (
+        <div key={topic.id}>
+            <Link href={paths.topic(topic.slug)}>
+                <Chip
+                    color='warning'
+                    variant='shadow'
+                    title={topic.slug}>
+                    {topic.slug}
+                </Chip>
+            </Link>
+        </div>
+    ));
+
+    return <div className='flex flex-row gap-2 flex-wrap'>{renderedTopics}</div>;
+}
